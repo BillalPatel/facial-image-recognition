82281285bcbeef59f7087becaa1b54ab287945c1
billalp <billal.patel@rightmove.co.uk>
2019-12-02 15:08:36 +0000

    Restyling and improving components

--- src/components/ImageLinkInput.js
+++ src/components/ImageLinkInput.js
@@ -10,10 +10,9 @@ const ImageLinkInput = ({ handleSubmit, loading }) => {
             <p>Submit Your Image URL Below</p>
             <div className="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
                 <fieldset className="cf bn ma0 pa0">
-                <legend className="pa0 f5 f4-ns mb3 black-80">Sign up for our newsletter</legend>
+                <legend className="pa0 f5 f4-ns mb3 black-80">Submit the URL of your image</legend>
                     <div className="cf">
-                        <label className="clip">Submit the URL of your image</label>
-                        <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Your Email Address" type="text" name="email-address"></input>
+                        <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="e.g. https://www.google.com/" type="text" name="url"></input>
                         <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="button" value="Detect!" onClick={handleSubmit}></input>
                     </div>
                 </fieldset>
--- src/components/Logo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Logo = () => {
-	return (
-		<div>Logo Image</div>
-	)
-}
-
-export default Logo;
