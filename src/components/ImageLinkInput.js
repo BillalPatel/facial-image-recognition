1fc4a3b7d25a5f4ae510f70330cdd0323a0a69f9
billalp <billal.patel@rightmove.co.uk>
2019-12-02 14:52:24 +0000

    Renaming things

--- src/components/ImageLinkInput.js
+++ src/components/ImageLinkInput.js
@@ -12,9 +12,9 @@ const ImageLinkInput = ({ handleSubmit, loading }) => {
                 <fieldset className="cf bn ma0 pa0">
                 <legend className="pa0 f5 f4-ns mb3 black-80">Sign up for our newsletter</legend>
                     <div className="cf">
-                        <label className="clip" htmlFor="email-address">Submit the URL of your image</label>
+                        <label className="clip">Submit the URL of your image</label>
                         <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Your Email Address" type="text" name="email-address"></input>
-                        <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="button" value="Subscribe" onClick={handleSubmit}></input>
+                        <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="button" value="Detect!" onClick={handleSubmit}></input>
                     </div>
                 </fieldset>
             </div>
