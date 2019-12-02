183ad29d7b5bcc0fda8b93faf4b98da96b405e9a
billalp <billal.patel@rightmove.co.uk>
2019-12-02 14:50:59 +0000

    Tachyons added

--- src/components/ImageLinkInput.js
+++ src/components/ImageLinkInput.js
@@ -1,7 +1,4 @@
 import React, { Fragment } from 'react';
-import 'antd/dist/antd.css';
-
-import { Input, Button } from 'antd';
 
 const ImageLinkInput = ({ handleSubmit, loading }) => {
     const enterLoading = () => {
@@ -11,20 +8,16 @@ const ImageLinkInput = ({ handleSubmit, loading }) => {
     return (
         <Fragment>
             <p>Submit Your Image URL Below</p>
-            <form onSubmit={handleSubmit}>
-                <Input
-                    placeholder="input search text"
-                    size="large"
-                    type="text"
-                    small="true"
-                    allowClear
-                    /*onSearch={value => console.log(value)}*/
-                />
-                <Button 
-                    onClick={handleSubmit}>
-                    Click me!
-                </Button>
-            </form>
+            <div className="bg-light-red mw7 center pa4 br2-ns ba b--black-10">
+                <fieldset className="cf bn ma0 pa0">
+                <legend className="pa0 f5 f4-ns mb3 black-80">Sign up for our newsletter</legend>
+                    <div className="cf">
+                        <label className="clip" htmlFor="email-address">Submit the URL of your image</label>
+                        <input className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Your Email Address" type="text" name="email-address"></input>
+                        <input className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="button" value="Subscribe" onClick={handleSubmit}></input>
+                    </div>
+                </fieldset>
+            </div>
         </Fragment>
     )
 }
