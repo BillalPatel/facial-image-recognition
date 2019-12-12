2055b0389b16628844d6403392faaabd385c3a3c
billalp <billal.patel@rightmove.co.uk>
2019-12-12 11:12:33 +0000

    Changed function to class

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,36 +1,41 @@
 import React from 'react';
 
-const SignInForm = ({onRouteChange}) => {
-  return (
-    <main className="pa4 black-80">
-      <div className="measure center">
-        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-          <legend className="f4 fw6 ph0 mh0">Sign In</legend>
-          <div className="mt3">
-            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-            <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+class SignInForm extends React.Component {
+  
+  
+  render() {
+    const {onRouteChange} = this.props;
+    return (
+      <main className="pa4 black-80">
+        <div className="measure center">
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f4 fw6 ph0 mh0">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+            </div>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+            </div>
+          </fieldset>
+          <div>
+            <input 
+              className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+              type="submit" 
+              value="Sign in"
+              onClick={() => onRouteChange('landing')}
+            />
           </div>
-          <div className="mv3">
-            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-            <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+          <div className="lh-copy mt3 hand">
+            <p className="f6 link dim black db" onClick={() => onRouteChange('signup')}>
+              Sign up
+            </p>
           </div>
-        </fieldset>
-        <div>
-          <input 
-            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-            type="submit" 
-            value="Sign in"
-            onClick={() => onRouteChange('landing')}
-          />
         </div>
-        <div className="lh-copy mt3 hand">
-          <p lassName="f6 link dim black db" onClick={() => onRouteChange('signup')}>
-            Sign up
-          </p>
-        </div>
-      </div>
-    </main>
-  )
+      </main>
+    )
+  }
 }
 
 export default SignInForm;
