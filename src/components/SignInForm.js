c3fb27566753627a2a06871adadb880c27c834eb
billalp <billal.patel@rightmove.co.uk>
2019-12-12 11:27:57 +0000

    Added signin functions

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 class SignInForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    }
+  }
   
+  onEmailChange = (event) => {
+    this.setState({signInEmail: event.target.value});
+  }
+
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
+
+  onSubmitSignIn = () => {
+
+  }
   
   render() {
     const {onRouteChange} = this.props;
@@ -12,11 +30,22 @@ class SignInForm extends React.Component {
             <legend className="f4 fw6 ph0 mh0">Sign In</legend>
             <div className="mt3">
               <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                type="email" 
+                name="email-address"
+                id="email-address"
+                onChange={this.onEmailChange}
+              />
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-              <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password"/>
+              <input 
+                className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                type="password"
+                name="password"
+                id="password"
+                onChange={this.onPasswordChange}
+              />
             </div>
           </fieldset>
           <div>
