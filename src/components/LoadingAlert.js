3f2f9904ab207b758a300933ef6ebe35c938c869
billalp <billal.patel@rightmove.co.uk>
2020-01-23 10:38:04 +0000

    Handle loading state

---
+++ src/components/LoadingAlert.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoaderIcon from 'react-loader-icon';
+
+const LoadingAlert = () => {
+  return (
+    <LoaderIcon size={100} />
+  );
+};
+
+export default LoadingAlert;
